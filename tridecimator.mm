287fecf871cbf1d1ac1850c34c32721a0a9c77b3
mizt <mizt@mizt.org>
2024-05-19 10:08:27 +0900

    Update

--- tridecimator.mm
+++ tridecimator.mm
@@ -1,13 +1,15 @@
+#import <Cocoa/Cocoa.h>
+
 // stuff to define the mesh
-#include <vcg/complex/complex.h>
+#import <vcg/complex/complex.h>
 
 // io
-#include <wrap/io_trimesh/import.h>
-#include <wrap/io_trimesh/export_obj.h>
+#import <wrap/io_trimesh/import.h>
+#import <wrap/io_trimesh/export_obj.h>
 
 // local optimization
-#include <vcg/complex/algorithms/local_optimization.h>
-#include <vcg/complex/algorithms/local_optimization/tri_edge_collapse_quadric.h>
+#import <vcg/complex/algorithms/local_optimization.h>
+#import <vcg/complex/algorithms/local_optimization/tri_edge_collapse_quadric.h>
 
 #include <vcg/complex/algorithms/clean.h>
 
@@ -38,7 +40,7 @@ struct MyUsedTypes: public UsedTypes<Use<MyVertex>::AsVertexType,Use<MyEdge>::As
 
 class MyVertex : public Vertex<MyUsedTypes, vertex::VFAdj, vertex::Coord3f, vertex::Mark, vertex::Qualityf, vertex::BitFlags> {
   public:
-    vcg::math::Quadric<double> &Qd() {return q;}
+    vcg::math::Quadric<double> &Qd() { return q; }
   private:
     math::Quadric<double> q;
 };
@@ -65,9 +67,10 @@ void Usage() {
 
 int main(int argc, char *argv[]) {
   
-  if(argc<4) Usage();
+  if(argc<3) Usage();
 
   MyMesh mesh;
+  
   int err = vcg::tri::io::Importer<MyMesh>::Open(mesh,argv[1]);
   if(err) {
     printf("Unable to open mesh %s : '%s'\n",argv[1],vcg::tri::io::Importer<MyMesh>::ErrorMsg(err));
@@ -78,110 +81,37 @@ int main(int argc, char *argv[]) {
   float ratio = atof(argv[3]);
   if(ratio>=1.0) ratio = 1.0;
   else if(ratio<=0.1) ratio = 0.1;
-  int FinalSize=mesh.fn*ratio;
+  int TargetFaceNum=mesh.fn*ratio;
   
   double TargetError = std::numeric_limits<double>::max();
-  bool CleaningFlag = false;
+  
+  bool CleaningFlag = true;
   
   TriEdgeCollapseQuadricParameter qparams;
-  qparams.QualityThr = 0.3;
-  qparams.QualityCheck = false;
-  qparams.HardQualityCheck = false; 
-  qparams.NormalCheck = false;
+  qparams.BoundaryQuadricWeight = 0.500000;
+  qparams.FastPreserveBoundary = false;
   qparams.AreaCheck = false;
-  qparams.OptimalPlacement = false;
-  qparams.ScaleIndependent = false;
-  qparams.PreserveBoundary = false;
+  qparams.HardQualityCheck = false;
+  qparams.HardQualityThr = 0.100000;
+  qparams.HardNormalCheck = false;
+  qparams.NormalCheck = false;
+  qparams.NormalThrRad = M_PI/2.0;
+  qparams.CosineThr = 0.000000; // ~ cos(pi/2)
+  qparams.OptimalPlacement = true;
+  qparams.SVDPlacement = false;
   qparams.PreserveTopology = false;
-  qparams.QualityQuadric = false;
+  qparams.PreserveBoundary = false;
+  qparams.QuadricEpsilon = 1e-15;
+  qparams.QualityCheck = true;
+  qparams.QualityThr = 0.300000;  // Collapsed that generate faces with quality LOWER than this value are penalized. So higher the value -> better the quality of the accepted triangles
+  qparams.QualityQuadric = false; // During the initialization manage all the edges as border edges adding a set of additional quadrics that are useful mostly for keeping face aspect ratio good.
+  qparams.QualityQuadricWeight = 0.001000; // During the initialization manage all the edges as border edges adding a set of additional quadrics that are useful mostly for keeping face aspect ratio good.
   qparams.QualityWeight = false;
-  
-  // parse command line.
-  for(int i=4; i<argc;) {
-    if(argv[i][0]=='-') {
-      switch(argv[i][1]) {
-        case 'Q':
-          qparams.QualityCheck = true;
-          printf("Using Quality Checking\n");
-          break;
-        case 'H':
-          qparams.HardQualityCheck = true;
-          printf("Using HardQualityCheck Checking\n");
-          break;
-        case 'N':
-          qparams.NormalCheck = true;
-          printf("Using Normal Deviation Checking\n");
-          break;
-        case 'A':
-          qparams.AreaCheck = true;
-          printf("Using Area Checking\n");
-          break;
-        case 'O':
-          qparams.OptimalPlacement = true;
-          printf("Using OptimalPlacement\n"); 
-          break;
-        case 'S':
-          qparams.ScaleIndependent = true;
-          printf("Using ScaleIndependent\n"); 
-          break;
-        case 'B':
-          qparams.PreserveBoundary = true;
-          printf("Preserving Boundary\n");
-          break;
-        case 'T':
-          qparams.PreserveTopology = true;
-          printf("Preserving Topology\n");
-          break;
-        case 'P':
-          qparams.QualityQuadric = true; 
-          printf("Adding Quality Quadrics\n");
-          break;
-        case 'W': qparams.QualityWeight = true;
-          printf("Using per vertex Quality as Weight\n");
-          break;
-        case 'p':
-          qparams.QualityQuadricWeight = atof(argv[i]+2);
-          printf("Setting QualityQuadricWeight factor to %f\n",qparams.QualityQuadricWeight);
-          break;
-        case 'w':
-          qparams.QualityWeightFactor = atof(argv[i]+2);
-          printf("Setting Quality Weight factor to %f\n",qparams.QualityWeightFactor);
-          break;
-        case 'q':
-          qparams.QualityThr = atof(argv[i]+2);
-          printf("Setting Quality Thr to %f\n",qparams.QualityThr);
-          break;
-        case 'h':
-          qparams.HardQualityThr = atof(argv[i]+2);
-          printf("Setting HardQuality Thr to %f\n",qparams.HardQualityThr);
-          break;
-        case 'n':
-          qparams.NormalThrRad = math::ToRad(atof(argv[i]+2)); 
-          printf("Setting Normal Thr to %f deg\n",qparams.NormalThrRad);
-          break;
-        case 'b':
-          qparams.BoundaryQuadricWeight = atof(argv[i]+2);
-          printf("Setting Boundary Weight to %f\n",qparams.BoundaryQuadricWeight); 
-          break;
-        case 'E':
-          qparams.QuadricEpsilon = atof(argv[i]+2);
-          printf("Setting QuadricEpsilon to %f\n",qparams.QuadricEpsilon); 
-          break;
-        case 'e':
-          TargetError = atof(argv[i]+2);
-          printf("Setting TargetError to %g\n",TargetError);
-          break;
-        case 'C':
-          CleaningFlag=true;
-          printf("Cleaning mesh before simplification\n");
-          break;
-        default:
-          printf("Unknown option '%s'\n", argv[i]);
-          exit(0);
-      }
-    }
-    i++;
-  }
+  qparams.QualityWeightFactor = 100.000000;
+  qparams.ScaleFactor = 1.000000;
+  qparams.ScaleIndependent = true;
+  qparams.UseArea = true;
+  qparams.UseVertexWeight = false;
   
   if(CleaningFlag) {
       int dup = tri::Clean<MyMesh>::RemoveDuplicateVertex(mesh);
@@ -189,7 +119,7 @@ int main(int argc, char *argv[]) {
       printf("Removed %i duplicate and %i unreferenced vertices from mesh \n",dup,unref);
   }
   
-  printf("reducing it to %i\n",FinalSize);
+  printf("reducing it to %i\n",TargetFaceNum);
 
   vcg::tri::UpdateBounding<MyMesh>::Box(mesh);
 
@@ -201,12 +131,13 @@ int main(int argc, char *argv[]) {
   int t2 = clock();
   printf("Initial Heap Size %i\n",int(DeciSession.h.size()));
 
-  DeciSession.SetTargetSimplices(FinalSize);
-  DeciSession.SetTimeBudget(0.5f);
-  DeciSession.SetTargetOperations(100000);
+  DeciSession.SetTargetSimplices(TargetFaceNum);
+  DeciSession.SetTimeBudget(0.1f); // this allows updating the progress bar 10 time for sec...
+  //DeciSession.SetTargetOperations(100000);
+
   if(TargetError< std::numeric_limits<float>::max()) DeciSession.SetTargetMetric(TargetError);
 
-  while(DeciSession.DoOptimization()&&mesh.fn>FinalSize&&DeciSession.currMetric<TargetError) {
+  while(DeciSession.DoOptimization()&&mesh.fn>TargetFaceNum&&DeciSession.currMetric<TargetError) {
     printf("Current Mesh size %7i heap sz %9i err %9g \n",mesh.fn, int(DeciSession.h.size()),DeciSession.currMetric);
   }
   
@@ -217,5 +148,6 @@ int main(int argc, char *argv[]) {
   mask|=vcg::tri::io::Mask::IOM_VERTCOORD;
   mask|=vcg::tri::io::Mask::IOM_FACEINDEX;
   vcg::tri::io::ExporterOBJ<MyMesh>::Save(mesh,argv[2],mask);
+    
   return 0;
 }
