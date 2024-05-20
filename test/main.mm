19b4d910e704573cc59252dda98089052b4bd3b8
mizt <mizt@mizt.org>
2024-05-20 14:54:33 +0900

    Update

--- test/main.mm
+++ test/main.mm
@@ -1,11 +1,56 @@
 #import <Cocoa/Cocoa.h>
 #import <vector>
 
+namespace FileManager {
+  
+  bool exists(NSString *path) {
+    static const NSFileManager *fileManager = [NSFileManager defaultManager];
+    NSError *err = nil;
+    [fileManager attributesOfItemAtPath:path error:&err];
+    return (err)?false:true;
+  }
+  
+  bool extension(NSString *str, NSString *ext) {
+    return ([[str pathExtension] compare:ext]==NSOrderedSame)?true:false;
+  }
+  
+  NSString *replace(NSString *str, NSString *from,NSString *to) {
+    return [str stringByReplacingOccurrencesOfString:from withString:to];
+  }
+  
+  NSString *replace(NSString *str,NSArray<NSString *> *from,NSString *to) {
+    NSMutableString *mstr = [NSMutableString stringWithString:str];
+    for(int n=0; n<[from count]; n++) {
+      mstr.string = replace(mstr,from[n],to);
+    }
+    return [NSString stringWithString:mstr];
+  }
+  
+  NSString *path(NSString *filename,NSString *identifier=nil) {
+    if(identifier==nil) {
+#if TARGET_OS_OSX
+      return [NSString stringWithFormat:@"%@/%@",[[NSBundle mainBundle] bundlePath],filename];
+#else
+      return [NSString stringWithFormat:@"%@/%@",[[NSBundle mainBundle] resourcePath],filename];
+#endif
+    }
+    else {
+      return [NSString stringWithFormat:@"%@/%@",[[NSBundle bundleWithIdentifier:identifier] resourcePath],filename];
+    }
+  }
+  
+  NSURL *URL(NSString *path) {
+    return [NSURL fileURLWithPath:path];
+  }
+}
+
+
+
 typedef int (*FILTER)(std::vector<float> *, std::vector<int> *, NSString *);
 
 int main(int argc, char *argv[]) {
   
-  NSString *src = [NSString stringWithContentsOfFile:@"../src.obj" encoding:NSUTF8StringEncoding error:nil];
+  NSString *src = [NSString stringWithContentsOfFile:@"../mesh.obj" encoding:NSUTF8StringEncoding error:nil];
   NSArray *lines = [src componentsSeparatedByCharactersInSet:[NSCharacterSet newlineCharacterSet]];
   NSCharacterSet *whitespaces = [NSCharacterSet whitespaceCharacterSet];
   
@@ -28,16 +73,26 @@ int main(int argc, char *argv[]) {
     }
   }
   
-  CFBundleRef bundle = CFBundleCreate(kCFAllocatorDefault,(CFURLRef)[NSURL fileURLWithPath:@"../tridecimator.plugin"]);
-  if(bundle) {
-    //NSLog(@"%@",bundle);
-    FILTER filter = (FILTER)CFBundleGetFunctionPointerForName(bundle,CFSTR("tridecimator"));
-    if(filter) {
-      filter(&v,&f,@"{\"ratio\":0.1}");
+  int pluginPass = 4;
+  NSString *pluginPath = @"../tridecimator.plugin";
+  CFStringRef pluginFunctionName = (__bridge CFStringRef)(@"tridecimator");
+  NSString *pluginParams = @"{\"ratio\":0.8}";
+  
+  if(FileManager::exists(pluginPath)) {
+    
+    while(pluginPass--) {
+      //NSLog(@"%d",pluginPass);
+      CFBundleRef bundle = CFBundleCreate(kCFAllocatorDefault,(CFURLRef)[NSURL fileURLWithPath:pluginPath]);
       if(bundle) {
-        filter = nullptr;
-        CFBundleUnloadExecutable(bundle);
-        CFRelease(bundle);
+        FILTER filter = (FILTER)CFBundleGetFunctionPointerForName(bundle,pluginFunctionName);
+        if(filter) {
+          filter(&v,&f,pluginParams);
+          if(bundle) {
+            filter = nullptr;
+            CFBundleUnloadExecutable(bundle);
+            CFRelease(bundle);
+          }
+        }
       }
     }
   }
